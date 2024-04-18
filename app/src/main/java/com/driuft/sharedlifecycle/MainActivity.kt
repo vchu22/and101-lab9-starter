565221aa71f828b7e8f8e010adbcbb9985defc74
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-04-17 23:07:48 -0400

    step 0

--- app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
+++ app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.driuft.sharedlifecycle
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Log statement at debug level
+        Log.d("Lifecycle: ", "onCreate")
+        // Toast
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStart() {
         super.onStart()
+        // Log statement at debug level
+        Log.d("Lifecycle: ", "onStart")
+        // Toast
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
