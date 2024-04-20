1327700967d5986a0bdb71374a4ff245c682efdb
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-04-19 23:06:13 -0400

    step 1-3

--- app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
+++ app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
@@ -1,26 +1,52 @@
 package com.driuft.sharedlifecycle
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var _sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Log statement at debug level
-        Log.d("Lifecycle: ", "onCreate")
-        // Toast
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+        _sharedPreferences = this.getSharedPreferences(this.packageName, Context.MODE_PRIVATE)
+
+        // Write the state
+        with (_sharedPreferences.edit()) {
+            // replace "onCreate" with the name of the method you're saving
+            putString(getString(R.string.current_lifecycle_state), "onCreate")
+            apply()
+        }
+
+        // Read the state
+        val currentState = _sharedPreferences.getString(
+            getString(R.string.current_lifecycle_state),
+            resources.getString(R.string.default_state)
+        ) ?: resources.getString(R.string.default_state)
+
+        // Log the state
+        Log.d("Lifecycle: ", currentState)
+        // Toast the state
+        Toast.makeText(this, currentState, Toast.LENGTH_SHORT).show()
     }
 
     override fun onStart() {
         super.onStart()
-        // Log statement at debug level
-        Log.d("Lifecycle: ", "onStart")
-        // Toast
-        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+        with (_sharedPreferences.edit()) {
+            putString(getString(R.string.current_lifecycle_state), "onStart")
+            apply()
+        }
+        // Read the state
+        val currentState = _sharedPreferences.getString(
+            getString(R.string.current_lifecycle_state),
+            resources.getString(R.string.default_state)
+        ) ?: resources.getString(R.string.default_state)
+        // Log the state
+        Log.d("Lifecycle: ", currentState)
+        // Toast the state
+        Toast.makeText(this, currentState, Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
